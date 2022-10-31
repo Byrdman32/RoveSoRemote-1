4df98be912932de13f79b06af623720467ad3062
Eli Byrd <edbgkk@mst.edu>
2022-10-31 10:49:29 -0500

    Update main.dart

--- rove_so_remote/lib/main.dart
+++ rove_so_remote/lib/main.dart
@@ -25,7 +25,6 @@ class RoveSoRemoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
         title: 'RoveSoRemote',
         theme: ThemeData(
           primarySwatch: Colors.red,
